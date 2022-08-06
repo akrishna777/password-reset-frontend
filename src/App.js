c867676c6364138a8d18b75fd09a2318b89e1319
Aravind Krishna <akaravindk59@gmail.com>
2022-08-06 14:22:54 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import { useState } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import ForgotPassword from './Pages/ForgotPassword'
+import Login from './Pages/Login'
+import Signup from './Pages/Signup'
+import Dashboard from './Pages/Dashboard'
+import EnterCode from './Pages/EnterCode'
+import ResetPassword from './Pages/ResetPassword'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/code" element={<EnterCode />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
